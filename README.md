f257a90a819d88db6b306979c508b2270d14acb4
Doshinsky <akter1997@gmail.com>
2018-10-04 17:14:19 +0700

    Added my version of GitHub aliases

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
 ### Note for .bashrc:
 Copy or move it at your home (~/) dir and type in terminal 'source ~/.bashrc'.
 After that you will be able to use all of mine aliases, e.g. use 'src' instead of 'source ~/.bashrc'.
+For GitHub aliases you should run script in, located in the same repo, 'gitaliases.sh'
+
+
+### Note for gitaliases.sh:
+Firstly, give rights on execution to the script with 'chomd +x ./gitaliases.sh'.
+After that you can run it from anywhere './gitaliases.sh'
+
+
+##### Star and Share if you like it :D
---
+++ gitaliases.sh
@@ -0,0 +1,15 @@
+git config --global alias.ch checkout
+git config --global alias.cm "commit -m"
+git config --global alias.st status
+git config --global alias.nb "checkout -b"
+git config --global alias.br "branch"
+git config --global alias.hist "log --pretty=format:'%C(cyan)%h%Creset %ad | %s%C(cyan)%d%Creset [%C(bold blue)%an%Creset] %C(green)(%cr)%Creset' --graph --date=short --all"
+git config --global alias.pu "push -u"
+git config --global alias.pl pull
+git config --global alias.ft fetch
+git config --global alias.mg merge
+git config --global alias.cf config
+git config --global alias.aa "add ./"
+git config --global alias.cl clone
+
+git config --global credential.helper store
