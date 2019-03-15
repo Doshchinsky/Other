fa6036d7516d72fa8ea01d914e344537d69ead72
Doshinsky <akter1997@gmail.com>
2019-03-15 21:28:33 +0700

    Some code customization

--- .postinstall.sh
+++ .postinstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Initializing post-install process..."
+echo -e "\n\e[1;33m==>\e[1;97m Initializing post-install process...\e[0;97m"
 if [ -e /boot/vmlinuz-linux ]
 then
   sudo mv /boot/vmlinuz-linux /boot/efi/EFI/Arch/vmlinuz-arch.efi
@@ -11,4 +11,4 @@ if [ -e /boot/amd-ucode.img ]
 then
   sudo mv /boot/amd-ucode.img /boot/efi/EFI/Arch/amd-ucode.img
 fi
-echo "Update complete!"
+echo -e "\e[1;32mUpdate and post-install operations were successfully completed!\e[0;97m\n"
