78ddb90ea2cbd324d2d703949afc10a70b6b7746
Doshinsky <akter1997@gmail.com>
2019-03-15 21:29:10 +0700

    Added useful script for repos update in one command. Enjoy it :3

---
+++ .repoupdate.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+echo -e "\n\e[1;33m==>\e[1;97m Initializing repos update process...\e[0;97m"
+DIR_LIST=`find $HOME/SibSUTIS -maxdepth 1 -type d`  # Change -maxdepth argument to the value you need
+UPD_REPO=0
+
+for DIR in ${DIR_LIST}; do
+  if [ -e ${DIR}/.git ]; then
+    cd ${DIR}
+    echo -ne "   \e[1;34m-> \e[1;97m"$(pwd)'\t[#-----------------------] 1%\r'
+    sleep 0.1
+    echo -ne "   \e[1;34m-> \e[1;97m"$(pwd)'\t[######------------------] 25%\r'
+    sleep 0.1
+    echo -ne "   \e[1;34m-> \e[1;97m"$(pwd)'\t[############------------] 50%\r'
+
+    git pull -q 2>/dev/null
+    if [ $? -eq 0 ]; then
+      UPD_REPO=$((${UPD_REPO} + 1))
+      echo -ne "   \e[1;34m-> \e[1;97m"$(pwd)'\t[##################------] 75%\r'
+      sleep 0.1
+      echo -ne "   \e[1;34m-> \e[1;97m"$(pwd)'\t[########################] 100%'
+      echo -ne '\n'
+    else
+      echo -ne "   \e[1;31m-> \e[1;97m"$(pwd)'\t[                        ] \e[1;31m???%'
+      echo -ne '\n'
+    fi
+  else
+    cd ${DIR}
+
+    echo -ne "   \e[1;34m-> \e[1;97m"$(pwd)'\t\t[#-----------------------] 1%\r'
+    sleep 0.1
+    echo -ne "   \e[1;34m-> \e[1;97m"$(pwd)'\t\t[######------------------] 25%\r'
+    sleep 0.1
+    echo -ne "   \e[1;34m-> \e[1;97m"$(pwd)'\t\t[############------------] 50%\r'
+
+    git pull -q 2>/dev/null
+    if [ $? -eq 0 ]; then
+      UPD_REPO=$((${UPD_REPO} + 1))
+      echo -ne "   \e[1;34m-> \e[1;97m"$(pwd)'\t\t[##################------] 75%\r'
+      sleep 0.1
+      echo -ne "   \e[1;34m-> \e[1;97m"$(pwd)'\t\t[########################] 100%'
+      echo -ne '\n'
+    else
+      echo -ne "   \e[1;31m-> \e[1;97m"$(pwd)'\t\t[                        ] \e[1;31m???%'
+      echo -ne '\n'
+    fi
+  fi
+done
+
+if [ ${UPD_REPO} -gt 0 ]; then
+  echo -e "\e[1;32m"${UPD_REPO} "repos were updated successfully...\e[0;97m\n"
+else
+  echo -e "\e[1;31m"${UPD_REPO} "repos were not updated...\e[0;97m\n"
+fi
--- README.md
+++ README.md
@@ -5,12 +5,25 @@
 ### Note for .bashrc:
 Copy or move it at your home (~/) dir and type in terminal `source ~/.bashrc`.
 After that you will be able to use all of mine aliases, e.g. use `src` instead of `source ~/.bashrc`.
-For GitHub aliases you should run script in, located in the same repo, `gitaliases.sh`
+For GitHub aliases you should run script in, located in the same repo, `.gitaliases.sh`
 
 
 ### Note for gitaliases.sh:
-Firstly, give rights on execution to the script with `chomd +x ./gitaliases.sh`.
+Firstly, give rights on execution to the script with `chomd +x .gitaliases.sh`.
 After that you can run simply it as `./gitaliases.sh`
+
+
+### Note for .repoupdate.sh:
+With this shell-script you can update whole your repositories in one command.
+Firstly, give rights on execution to the script with `chomd +x .repoupdate.sh`.
+Place this script in the folder with your repos and run it (`./.repoupdate.sh`).
+
+
+### Note for .postinstall.sh:
+This script designed to fix errors, which occurs while using rEFInd instead of the standart GRUB.
+Firstly, give rights on execution to the script with `chomd +x .postinstall.sh`.
+Now after every core-update you need to run this script (`./.postinstall.sh`).
+Also check out `.bashrc` file to get usefull alias for Arch-Linux core update.
 
 
 ##### Star and Share if you like it :D
