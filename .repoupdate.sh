aa70c78b6139d1a827a362b453597a476a493c76
Doshinsky <akter1997@gmail.com>
2019-03-23 11:06:44 +0700

    Added the dumb-checker (r u online or not)

--- .repoupdate.sh
+++ .repoupdate.sh
@@ -4,6 +4,12 @@ echo -e "\n\e[1;33m==>\e[1;97m Initializing repos update process...\e[0;97m"
 DIR_LIST=`find $HOME/SibSUTIS -maxdepth 1 -type d`  # Change -maxdepth argument to the value you need
 UPD_REPO=0
 
+ping -c2 -i0.2 8.8.8.8 >/dev/null 2>/dev/null
+if [[ $? -eq 2 ]]; then
+  echo -e "   \e[1;31mAn error with connection was occured... Exiting now...\e[0;97m\n"
+  exit -1
+fi
+
 for DIR in ${DIR_LIST}; do
   if [ -e ${DIR}/.git ]; then
     cd ${DIR}
